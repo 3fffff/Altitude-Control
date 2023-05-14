1fcfa075024cd5d215dabbe70e9437c8226dde82
3fffff <g3g3g3g3g33@yandex.ru>
2023-05-14 15:36:46 +0500

    Update variables.m

--- variables.m
+++ variables.m
@@ -1,6 +1,10 @@
-Ixx =1;
-Iyy =1;
-Izz =1;
-L =1;
-N = 1;
-M =1;
+M=0.9928 %Mass of the vehicle
+L=0.24%Length of the arm
+Izz=(1/3)*M*L^2% 
+g=9.8
+Ixx=0.09
+Iyy=0.09
+omega =260
+b=(M*g/4)/(omega^3)%Thrust Coefficient Thrust=thrust coefficient x motor rotation speed
+d=2.0810*(10^(-6)) % Drag Torque Coefficient
+Jr=0.04439
